451a4571e43f784cf2fa415dc6b3188d1ee3e884
JoicePC <joicebf1995@outlook.com>
2023-09-20 10:26:37 -0300

    pc pro not

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import gifImage from '../imagens/cuei.gif';
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div>
             <footer>
+                <img width={101}
+                  height={110} 
+                  src={gifImage} 
+                  alt="GIF" />
                 <p className="copyright">&copy; Copyright- Joice - 2023</p>
             </footer>
         </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Carousel from 'react-bootstrap/Carousel';
 
 function HomePage() {
-    const imgPath = require("D:/Faculdade/PW/portifolioreact/src/imagens/Joice.jpg");
+  const imgPath = require("D:/Faculdade/PW/portifolioreact/src/imagens/Joice.jpg"),
+    img1 = require("D:/Faculdade/PW/portifolioreact/src/imagens/carrosel/imagem1.jpg"),
+    img2 = require("D:/Faculdade/PW/portifolioreact/src/imagens/carrosel/imagem2.jpg"),
+    img3 = require("D:/Faculdade/PW/portifolioreact/src/imagens/carrosel/imagem3.jpg");
+
   return (
     <div className="dados">
+
       <div className="container mt-5">
         <div className="text-center">
-        <img
-            id="imagem"
-            src={imgPath}
-            width="100%"
-            height="auto"
-            object-fit="cover"
-            className="centered-image"
-            alt="Joice Barros"
-          />
+          <Container>
+            <Row>
+              <Col xs={6} md={4} >
+                <Image id="imagem"
+                  width={171}
+                  height={180} src={imgPath} roundedCircle />
+              </Col>
+            </Row>
+          </Container>
         </div>
-        <br />
-        <br />
+
         <h2>Quem sou</h2>
         <p>
           <br />
@@ -41,41 +50,35 @@ function HomePage() {
           apresentada a alguns tipos de teste e treinada na parte teórica, embora ainda não tenha
           colocado a mão na massa, é uma área que quero continuar explorando.
         </p>
-          <h2>Formação</h2>
-          <ul>
-            <br />
-            <li><b>2018 - Presente:</b> Engenharia da Computação, Centro Federal de Educação Tecnológica
-              de Minas Gerais, Campus Timóteo.</li>
-            <br />
-            <li><b>2016:</b> Curso de NR10, Colégio CEST, Ipatinga-MG.</li>
-            <br />
-            <li><b>2013 - 2014:</b> Curso Técnico em Eletrotécnica, Colégio CEST, Ipatinga - MG.</li>
-            <br />
-          </ul>
-          <br />
-          <h2>Tecnologias</h2>
-          <ul>
-            <br />
-            <li><b>Avançado:</b> Java, Gimp, Google Docs.</li>
-            <br />
-            <li><b>Intermediário:</b> C, Latex, HTML5, Teste de Software, VHDL, HTML, CSS, JUnit.</li>
-            <br />
-            <li><b>Básico:</b> Linux, Arduino IDE, SQL, MySQL, Assemble, Teste de Integração,
-              Google Sheets, AutoCad.</li>
-            <br />
-          </ul>
-          <br />
-          <h2>Idiomas</h2>
-          <ul>
-            <br />
-            <li><b>Inglês:</b> Leitura intermediária, conversação básica.</li>
-            <br />
-            <li><b>Espanhol:</b> Conversação intermediária, leitura intermediária.</li>
-            <br />
-          </ul>
+        <div>
+          <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={img1}
+                alt="Primeiro slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={img2}
+                alt="Primeiro slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={img3}
+                alt="Primeiro slide"
+              />
+              
+            </Carousel.Item>
+          </Carousel>
         </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default HomePage;
